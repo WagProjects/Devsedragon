d0c1471a669798fd575dbe8bbb2b4869549ccbfb
Wagner Oliveira <contato-wagneros@outlook.com>
2023-06-09 17:26:04 -0300

    Class mago e arqueiro

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { Personagem } from "../modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "../modules/mago.js"
+import { Arqueiro } from "../modules/arqueiro.js"
+import { ArqueiroMago } from "../modules/arqueiro-mago.js"
 
-const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
-const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
+const magoAntonio = new Mago('Antonio', 10, 'Gelo', 8, 15)
+const magaJulia = new Mago("Julia", 8, 'Fogo', 9, 20)
+const arqueiroCascao = new Arqueiro("Cascão", 18, 20)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'Ar', 4, 8)
 
-const personagens = [personagemPedrinho, personagemJose]
+
+const personagens = [magoAntonio, magaJulia, arqueiroCascao, arqueiroMagoChico]
 
 new PersonagemView(personagens).render()
